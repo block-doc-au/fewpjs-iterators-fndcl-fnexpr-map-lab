cb53199b0221612b6cb69257546871554559fa79
block-doc-au <neilc@cns-gc.com.au>
2022-08-21 21:17:13 +1000

    NC Completed Array Map lab

--- index.js
+++ index.js
@@ -13,5 +13,40 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+  let newTutorials = tutorials.map(name => toTitleCase(name));
+  return newTutorials
 }
+
+console.log(titleCased);
+
+// function toTitleCase(name){
+//   return (name.toUpperCase());
+// };
+
+function toTitleCase(str) {
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      //return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      return txt.charAt(0).toUpperCase() + txt.substr(1);
+    }
+  );
+}
+
+// const titleCased = tutorials.map(name => toTitleCase(name));
+  
+
+// console.log(titleCased);
+
+
+// function toTitleCase(str) {
+//     return str.replace(
+//     /\w*/g,
+//     function(txt) {
+//       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+//     }
+//   );
+// }
+
+
+
